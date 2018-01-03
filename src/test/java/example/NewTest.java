67be37f2b6530dcc9816fa75c255651b77929b2d
Devarajd.relevance <Devarajd.relevance@redington.dxb>
2018-01-03 09:30:02 +0000

    first commit

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,52 @@
+package example;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class NewTest implements t {
+
+	@Test
+	  public void testgooglrsearch() throws InterruptedException{
+		
+		System.setProperty("webdriver.gecko.driver","D:\\Users\\Devarajd.relevance\\Downloads\\geckodriver.exe\\");
+		
+		FirefoxProfile ffProfile= new FirefoxProfile();	
+		ffProfile.setAcceptUntrustedCertificates(true);
+		ffProfile.setAssumeUntrustedCertificateIssuer(false);
+		DesiredCapabilities dc = new FirefoxOptions().setProfile(ffProfile).addTo(DesiredCapabilities.firefox()); 
+		WebDriver driver = new FirefoxDriver (dc);
+		driver.manage().window().maximize();
+
+		Thread.sleep(2000);
+
+		// Open URL
+		driver.get("https://test.redington.market");
+	  
+		// Enter UserName 
+		driver.findElement(By.xpath(".//*[@id='username']")).sendKeys("pm20@mailinator.com");
+		
+		// Enter Password 
+		driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("Pass@123");
+				
+		Thread.sleep(10000);
+
+		 //This will read the CAPTCHA 
+         String captchaValue = driver.findElement(By.xpath("html/body/app-root/ng-component/app-sign-in/section/div/div/div[2]/form/div[3]/div/label/div"
+)).getText();  
+		 System.out.println(captchaValue);
+        driver.findElement(By.xpath(".//*[@id='captcha']")).sendKeys(captchaValue);
+        
+     // Click the Sign in button
+        
+        driver.findElement(By.xpath("html/body/app-root/ng-component/app-sign-in/section/div/div/div[2]/form/button")).click();
+
+        System.out.println(driver.getTitle());
+
+		//driver.quit();
+}
+}
